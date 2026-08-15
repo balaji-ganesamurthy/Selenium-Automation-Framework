bbeabe6ed76e01dc4e89f26fb0b2706379775e68
Balaji G <balajigofficial@gmail.com>
2026-08-16 00:12:57 +0530

    Add environment management

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -5,37 +5,50 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-	
-	private static Properties prop;
-	
-	static {
-		try {
-			FileInputStream fis = new FileInputStream("src/test/resources/config/config.properties");
-			prop = new Properties();
-			prop.load(fis);
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static String getProperty(String key) {
-		return prop.getProperty(key);
-	}
-	
-	public static String getBrowser() {
-		return getProperty("browser");
-	}
-	
-	public static String getURL() {
-		return getProperty("url");
-	}
-	
-	public static int getTimeOut() {
-		return Integer.parseInt(getProperty("timeout"));
-	}
-	
-	public static boolean isHeadless() {
-	    return Boolean.parseBoolean(getProperty("headless"));
-	}
 
+    private static Properties prop;
+
+    static {
+        try {
+
+            // Load environment selector
+            FileInputStream envFile =
+                    new FileInputStream("src/test/resources/config/config.properties");
+
+            Properties envProperties = new Properties();
+            envProperties.load(envFile);
+
+            String environment = envProperties.getProperty("environment");
+            
+            FileInputStream configFile =
+                    new FileInputStream(
+                            "src/test/resources/config/config-" + environment + ".properties");
+
+            prop = new Properties();
+            prop.load(configFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+
+    public static String getBrowser() {
+        return getProperty("browser");
+    }
+
+    public static String getURL() {
+        return getProperty("url");
+    }
+
+    public static int getTimeOut() {
+        return Integer.parseInt(getProperty("timeout"));
+    }
+
+    public static boolean isHeadless() {
+        return Boolean.parseBoolean(getProperty("headless"));
+    }
 }
