c99f0f658bb48ce278f9d3b03f7addee112c3a49
Balaji G <balajigofficial@gmail.com>
2026-09-10 16:25:08 +0530

    Generate Cucumber JSON report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features", 
                  glue = { "hooks", "stepDefinitions" }, 
-                 plugin = { "pretty", "html:reports/cucumber-report.html" },
+                 plugin = { "pretty", "html:reports/cucumber-report.html" , "json:reports/cucumber.json"},
                  tags = "@smoke")
 public class TestRunner extends AbstractTestNGCucumberTests {
 
